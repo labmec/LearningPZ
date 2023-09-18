c37a911354abada507069959230bbf5ced5871cf
ViniciusGiacominiSantos <vinigs3232@gmail.com>
2023-09-18 09:35:54 -0300

    feat CMake program

---
+++ build/CMakeFiles/pztest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pztest.
+# This may be replaced when dependencies are built.
--- pztest.cpp
+++ pztest.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "pzgmesh.h"
 
 using namespace std;
 
 int main() {
     std::cout << "Oi mundo" << std::endl;
+    TPZGeoMesh* gmesh = new TPZGeoMesh();
     return 0;
 }
